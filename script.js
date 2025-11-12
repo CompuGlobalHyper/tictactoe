095eb370cc8406e58f224908b4a6f57198029f2e
CompuGlobalHyper <phineascrisp@gmail.com>
2025-11-12 15:30:41 -0800

    Added css styling, completed functionality, and accounted for ties and other edge cases

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 (function (){
     const Gameboard = {
-        gameState: [['X', 'X', 'X'],
+        gameState: [['_', '_', '_'],
                     ['_', '_', '_'],
                     ['_', '_', '_']],
 
         resetGame: function () {
-            this.gameState = [['_', '_', '_'],
-                              ['_', '_', '_'],
-                              ['_', '_', '_']]
-            this.gameOver = false
+            const resetButton = document.querySelector('#reset-button')
+            const gameLog = document.getElementById('log')
+            resetButton.addEventListener('click', () => {
+                console.log("you have reset")
+                this.playerOne = ''
+                this.playerTwo = ''
+                this.isX = true
+                this.gameStarted = false
+                this.gameOver = true
+                let newDiv = document.createElement('div')
+                gameLog.appendChild(newDiv)
+                newDiv.textContent = `> You have reset the game. Please choose your player names...`
+                Gameboard.cleanLog()
+                this.gameState = [['_', '_', '_'],
+                                ['_', '_', '_'],
+                                ['_', '_', '_']]
+                const gameSquares = document.querySelectorAll('.square')
+                gameSquares.forEach(function(gamePiece) {
+                    gamePiece.children[0].textContent = " "
+                })
+            })
         },
 
         gameOver: false,
+        isX : true,
+        gameStarted: false,
+        disableGame: false,
+        playerOne: '',
+        playerTwo: '',
+        playerOneFound : false,
+        playerTwoFound : false,
+        logCounter : 0,
 
         displayGame: function () {
             for (row of this.gameState) {
@@ -20,9 +45,25 @@
         },
 
         createPlayers: function () {
-            const playerOne = { name: 'Joe', score: 0 }
-            const playerTwo = { name: 'Jane', score: 0 }
-            return { playerOne, playerTwo }
+            const playerButton = document.querySelectorAll('button')
+            playerButton.forEach((button) => {
+                button.addEventListener('click', (event) => {
+                    event.preventDefault()
+                    if (!Gameboard.gameStarted) {
+                        if (button.id === 'player-one-button') {
+                            const submission = document.getElementById('player-one').value
+                                this.playerOne = submission
+                                console.log(submission)
+                                this.playerOneFound = true
+                        }
+                        if (button.id === 'player-two-button') {
+                                const submission = document.getElementById('player-two').value
+                                this.playerTwo = submission
+                                this.playerTwoFound = true
+                        }
+                    }
+                })
+            })
         },
 
         chooseSymbol: function (){
@@ -66,79 +107,221 @@
             return null
         },
 
-        printWinner: function (player) {
-            console.log(`Congrats ${player.name}!!! You won the game! :)`)
+        checkTie: function (board) {
+            for (let row of board) {
+                for (let square of row) {
+                    if (square === "_") return false
+                }
+            }
+            return true
+        },
+
+        printWinner: function () {
+            const gameLog = document.getElementById('log')
+            let newDiv = document.createElement('div')
+            gameLog.appendChild(newDiv)
+            if (this.isX && !this.gameOver) {
+                newDiv.textContent = `> Congrats ${this.playerOne}!!! You won the game! :)`
+                Gameboard.cleanLog()
+                this.gameOver = true
+            }
+            else if (!this.gameOver){
+                newDiv.textContent = `> Congrats ${this.playerTwo}!!! You won the game! :)`
+                Gameboard.cleanLog()
+                this.gameOver = true
+            }
+            if (this.gameOver && !this.disableGame) {
+            this.disableGame = true
+            let newDiv2 = document.createElement('div')
+            gameLog.appendChild(newDiv2)
+            newDiv2.textContent = `> Please click 'Reset' to start selecting new player names`
+            Gameboard.cleanLog()
+            }
+            this.playerOneFound = false
+            this.playerTwoFound = false
+        },
+
+        printTie: function () { 
+            const gameLog = document.getElementById('log')
+            let newDiv = document.createElement('div')
+            newDiv.textContent = `> It was a tie, oh well...`
+            gameLog.appendChild(newDiv)
+            Gameboard.cleanLog()
             this.gameOver = true
+            this.disableGame = true
+            let newDiv2 = document.createElement('div')
+            gameLog.appendChild(newDiv2)
+            newDiv2.textContent = `> Please click 'Reset' to start selecting new player names`
+            Gameboard.cleanLog()
         },
 
         checkWinner: function (board) {
             if (this.checkRows(board)) {
-                if (this.checkRows(board) === this.playerOne.symbol) {
-                    this.playerOne.score++
-                    this.printWinner(this.playerOne)
+                if (this.checkRows(board) === 'X') {
+                    this.printWinner()
                 } else {
-                    this.playerTwo.score++
-                    this.printWinner(this.playerTwo)
+                    this.printWinner()
                 }
             }
             else if (this.checkColumns(board)) {
-                if (this.checkColumns(board) === this.playerOne.symbol) {
-                    this.playerOne.score++
-                    this.printWinner(this.playerOne)
+                if (this.checkColumns(board) === 'X') {
+                    this.printWinner()
                 } else {
                     this.playerTwo.score++
-                    this.printWinner(this.playerTwo)
+                    this.printWinner()
                 }
             }
             else if (this.checkDiagonals(board)) {
-                if (this.checkDiagonals(board) === this.playerOne.symbol) {
-                    this.playerOne.score++
-                    this.printWinner(this.playerOne)
+                if (this.checkDiagonals(board) === 'X') {
+                    this.printWinner()
                 } else {
-                    this.playerTwo.score++
-                    this.printWinner(this.playerTwo)
+                    this.printWinner()
                 }
-            } 
+            } else if (this.checkTie(board)) {
+                console.log("TIE")
+                this.printTie()
+            }
+        },
+
+        logGame: function () {
+            const gameLog = document.getElementById('log')
+            const gameSquares = document.querySelectorAll('.square')
+            gameSquares.forEach(function(gamePiece) {
+                gamePiece.addEventListener('click', function() {
+                    if (Gameboard.gameStarted) {
+                        console.log(Gameboard.playerOne)
+                        console.log(gamePiece.children[0].textContent)
+                        if (Gameboard.playerOneFound && Gameboard.playerTwoFound) {
+                            if ((gamePiece.children[0].textContent === 'X' ||
+                                gamePiece.children[0].textContent === 'O') &&
+                                !Gameboard.gameOver) {
+                                    let newDiv = document.createElement('div')
+                                    gameLog.appendChild(newDiv)
+                                    newDiv.textContent = '> Invalid move, please try again.'
+                                    Gameboard.cleanLog()
+                                }  
+                            else if (Gameboard.isX && !Gameboard.gameOver) {
+                                let newDiv = document.createElement('div')
+                                gameLog.appendChild(newDiv)
+                                newDiv.textContent = `> ${Gameboard.playerOne} has placed an 'X'`
+                                Gameboard.cleanLog()
+
+                            }
+                            else if (!Gameboard.gameOver) {
+                                let newDiv = document.createElement('div')
+                                gameLog.appendChild(newDiv)
+                                newDiv.textContent = `> ${Gameboard.playerTwo} has placed an 'O'`
+                                Gameboard.cleanLog()
+                            }
+                        }
+                    }
+                })
+            })
+        },
+
+        chooseMove: function () {
+            const gameSquares = document.querySelectorAll('.square')
+            gameSquares.forEach(function(gamePiece) {
+                gamePiece.addEventListener('click', function() {
+                    console.log(Gameboard.isX)
+                    if (gamePiece.children[0].textContent === 'X' ||
+                        gamePiece.children[0].textContent === 'O') {
+                            return
+                        }  
+                    else if (Gameboard.isX && !Gameboard.gameOver) {
+                        const row = gamePiece.children[0].dataset.row
+                        const col = gamePiece.children[0].dataset.col
+                        gamePiece.children[0].textContent = 'X'
+                        Gameboard.gameState[row][col] = 'X'
+
+                    }
+                    else if (!Gameboard.gameOver) {
+                        const row = gamePiece.children[0].dataset.row
+                        const col = gamePiece.children[0].dataset.col
+                        gamePiece.children[0].textContent = 'O'
+                        Gameboard.gameState[row][col] = 'O'
+                    }
+                    if (!Gameboard.gameOver) {
+                        Gameboard.checkWinner(Gameboard.gameState)
+                        Gameboard.isX = !Gameboard.isX
+                    }
+                    
+                })
+            })
+        },
+
+        logPlayers: function () {
+            const gameLog = document.getElementById('log')
+            const gameLogCounter = 0
+            const playerButton = document.querySelectorAll('button')
+                playerButton.forEach((button) => {
+                    button.addEventListener('click', (event) => {
+                        event.preventDefault()
+                        if (!Gameboard.gameStarted) {
+                            if (button.id === 'player-one-button') {
+                                const submission = document.getElementById('player-one').value
+                                if (submission) {
+                                    let newDiv = document.createElement('div')
+                                    gameLog.appendChild(newDiv)
+                                    newDiv.textContent = `> Player One is named ${submission} and will use 'X'`
+                                    Gameboard.cleanLog()
+                                }
+                            }
+                            if (button.id === 'player-two-button') {
+                                const submission = document.getElementById('player-two').value
+                                if (submission) {
+                                    let newDiv = document.createElement('div')
+                                    gameLog.appendChild(newDiv)
+                                    newDiv.textContent = `> Player Two is named ${submission} and will use 'O'`
+                                    Gameboard.cleanLog()
+                                }
+                            }
+                        }
+                    })
+                })
+        },
+        
+        startGame: function () {
+            startButton = document.querySelector('#start-button')
+            startButton.addEventListener('click', () => {
+                console.log("You clicked on start")
+                if (this.playerOneFound && this.playerTwoFound && !this.gameStarted) {
+                    const gameLog = document.getElementById('log')
+                    let newDiv = document.createElement('div')
+                    gameLog.appendChild(newDiv)
+                    newDiv.textContent = '> Game is ready to play!'
+                    this.gameOver = false
+                    this.gameStarted = true
+                    Gameboard.cleanLog()
+                    this.chooseMove()
+                }
+            })
         },
 
         init: function () {
-            const { playerOne, playerTwo } = this.createPlayers()
-            this.playerOne = playerOne
-            this.playerTwo = playerTwo
-            this.chooseSymbol()
-        },
-
-        chooseMove: function (player) {
-            let row = prompt('Which row do you choose? 0, 1, or 2')
-            let column = prompt('Which column do you choose? 0, 1, or 2')
-            if (this.gameState[row][column] === '_'){
-                console.log(player.symbol)
-                this.gameState[row][column] = player.symbol
-                console.log(this.gameState[row][column])
-            }
-            else {
-                alert('Invalid move!')
-                this.chooseMove(player)
-            }
-            this.checkWinner(this.gameState)
-        },
-
-        playGame: function () {
-            while (!this.gameOver) {
-                this.displayGame()
-                this.chooseMove(this.playerOne)
-                if (!this.gameOver) {
-                    this.displayGame()
-                    this.chooseMove(this.playerTwo)
-                }
-                else break
-            }
+            console.log("Displaying game")
+            this.displayGame()
+            console.log("Creating players")
+            this.createPlayers()
+            console.log("Initializing player log")
+            this.logPlayers()
+            console.log ("Game log is ready")
+            this.logGame()
+            console.log("Game ready to start")
+            this.startGame()
             this.resetGame()
-
+            console.log("reset button online")
         },
+
+        cleanLog: function () {
+            console.log(this.logCounter)
+            const log = document.querySelector('#log')
+            if (this.logCounter > 2) {
+                log.children[0].remove()
+            }
+            else this.logCounter += 1;
+        }
 
     }
-    Gameboard.init()
-    Gameboard.playGame()
-
+Gameboard.init()
 })();
